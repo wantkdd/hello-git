1440be1f62647ba06c3ed6f629ffff3efc54a7ae
Sangwan Yu <ryusangwan12@hansung.ac.kr>
2024-12-03 20:25:34 +0900

    하이 추가

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("hello!");
         System.out.println("안녕");
+        System.out.println("하이");
     }
 }
